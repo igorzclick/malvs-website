59349b76ce68bb4bca29d201880dcacdc3dcdf98
igor almeida <ii.igoralmeida@gmail.com>
2025-02-24 20:00:18 -0300

    troca de cor e texto da pagina principal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,21 +11,10 @@ const Header = () => {
             <div className="banner-container container">
                 <div className="banner-texts">
                     <h1 className="banner-title">
-                        <span>Providentia Consultoria</span>
+                        <span>Somos Responsaveis</span>
                     </h1>
                     <h2 className='banner-subtitle' aria-label='defenda o que é seu, fortaleça sua marca e proteja seu futuro hoje!'>
-                        <span>Defenda&nbsp;</span>
-                        <span>o que&nbsp;</span>
-                        <span>é&nbsp;</span>
-                        <span>seu,</span> <br />
-                        <span>fortaleça&nbsp;</span>
-                        <span>sua&nbsp;</span>
-                        <span>marca&nbsp;</span>
-                        <span>e</span> <br />
-                        <span>proteja&nbsp;</span>
-                        <span>seu&nbsp;</span>
-                        <span>futuro&nbsp;</span>
-                        <span>hoje!</span>
+                       <p> pela sua <strong>inovação</strong></p>
                     </h2>
                 </div>
                 <div className="button">
